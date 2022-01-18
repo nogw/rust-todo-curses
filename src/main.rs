2edace521568c6f9fd85bef657aaaadcd6cdc145
nogw <gabrielnogueiraoliveira@gmail.com>
2022-01-18 01:15:20 -0300

    bar

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 extern crate ncurses;
 
+use chrono;
 use ncurses::*;
 use std::fs::File;
 use std::io::{self, BufRead, ErrorKind, Write};
 
-const COLOR_BACKGROUND: i16 = 15;
-const COLOR_KEYWORD: i16 = 0;
-const COLOR_REGULAR_PAIR: i16 = 0;
-const COLOR_PAIR_HIGHLIGHT: i16 = 2;
+const REGULAR_PAIR: i16 = 0;
+const HIGHLIGHT_PAIR: i16 = 1;
 
 // constants::KEY_UP
 // constants::KEY_DOWN
@@ -47,16 +46,19 @@ impl Ui {
           }
           *cursor += 1
         }
+
         constants::KEY_LEFT => {
           if *cursor > 0 {
             *cursor += 1
           }
         }
+
         constants::KEY_RIGHT => {
           if *cursor < buffer.len() {
             *cursor += 1
           }
         }
+
         constants::KEY_BACKSPACE => {
           if *cursor > 0 {
             *cursor -= 1;
@@ -65,27 +67,29 @@ impl Ui {
             }
           }
         }
+
         constants::KEY_DC => {
           if *cursor < buffer.len() {
             buffer.remove(*cursor);
           }
         }
+
         _ => self.key = Some(key),
       }
     }
 
     {
       mv(self.height - 1, 0);
-      attron(COLOR_PAIR(COLOR_REGULAR_PAIR) | A_BOLD());
+      attron(COLOR_PAIR(0) | A_BOLD());
       addstr(buffer);
-      attroff(COLOR_PAIR(COLOR_REGULAR_PAIR) | A_BOLD());
+      attroff(COLOR_PAIR(0) | A_BOLD());
     }
 
     {
       mv(self.height - 1, 0 + *cursor as i32);
-      attron(COLOR_PAIR(COLOR_PAIR_HIGHLIGHT));
+      attron(COLOR_PAIR(1));
       addstr(buffer.get(*cursor..=*cursor).unwrap_or(" "));
-      attroff(COLOR_PAIR(COLOR_PAIR_HIGHLIGHT));
+      attroff(COLOR_PAIR(1));
     }
   }
 }
@@ -180,13 +184,14 @@ fn save_todos(todos: &[(TodoStatus, String)], file_path: &str) {
 fn main() {
   initscr();
   noecho();
+  keypad(stdscr(), true);
   timeout(16);
-  // keypad(stdscr(), true);
+  curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
 
   start_color();
-  init_pair(COLOR_REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
-  init_pair(COLOR_PAIR_HIGHLIGHT, COLOR_KEYWORD, COLOR_BACKGROUND);
-  curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+  init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
+  init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
+
   let mut quit = false;
   let mut ui = Ui::default();
 
@@ -196,7 +201,7 @@ fn main() {
   let mut editing = false;
   let mut editing_cursor = 0;
 
-  match load_todos(&mut todos, "teste.txt") {
+  match load_todos(&mut todos, "todos.txt") {
     Ok(()) => (),
     Err(err) => {
       if err.kind() == ErrorKind::NotFound {
@@ -212,14 +217,16 @@ fn main() {
     erase();
     ui.layout();
 
-    attron(COLOR_PAIR(2));
-    mvaddstr(
-      ui.height - 2,
-      0,
-      &format!("todos: {} | current: {}", todos.len(), todo_curr + 1),
+    // let (_, at) = &todos[todo_curr];
+    let header = subwin(stdscr(), 1, 80, ui.height - 2, 0);
+    wattron(header, COLOR_PAIR(1) | A_BOLD());
+    waddstr(
+      header,
+      &format!("todos: {} - current: {}", todos.len(), "at"),
     );
+    wbkgd(header, COLOR_PAIR(1) | A_BOLD());
+    wattroff(header, COLOR_PAIR(1) | A_BOLD());
 
-    attroff(COLOR_PAIR(2));
     for (index, (marked, content)) in todos.iter_mut().enumerate() {
       mv(index as i32, 0);
 
@@ -232,12 +239,15 @@ fn main() {
         },
         content
       );
+
       if index == todo_curr {
-        attron(COLOR_PAIR(2) | A_BOLD());
+        attron(COLOR_PAIR(1) | A_BOLD());
         addstr(todo);
-        attroff(COLOR_PAIR(2) | A_BOLD());
+        attroff(COLOR_PAIR(1) | A_BOLD());
+
         if editing {
           ui.edit_field(content, &mut editing_cursor);
+
           if let Some('\n') = ui.key.map(|x| x as u8 as char) {
             editing = false
           }
@@ -249,21 +259,19 @@ fn main() {
 
     if let Some(key) = ui.key.take() {
       match key as u8 as char {
-        'A' => uplist(&todos, &mut todo_curr),
-        'B' => dwlist(&todos, &mut todo_curr),
-        'D' => marktd(&mut todos, todo_curr),
-        'C' => delete(&mut todos, &mut todo_curr),
-        ';' => {
+        'j' => uplist(&todos, &mut todo_curr),
+        'k' => dwlist(&todos, &mut todo_curr),
+        'd' => marktd(&mut todos, todo_curr),
+        'a' => delete(&mut todos, &mut todo_curr),
+        'e' => {
+          // let time = chrono::offset::Local::now().format("%b %d %H:%M:%S");
           todos.insert(0, (TodoStatus::Todo, String::new()));
           editing_cursor = 0;
           editing = true;
         }
+        'q' => quit = true,
         _ => ui.key = Some(key),
       }
-    }
-
-    if let Some('q') = ui.key.map(|x| x as u8 as char) {
-      quit = true
     }
 
     let key = getch();
@@ -273,6 +281,6 @@ fn main() {
     }
   }
 
-  save_todos(&todos, "teste.txt");
+  save_todos(&todos, "todos.txt");
   endwin();
 }
